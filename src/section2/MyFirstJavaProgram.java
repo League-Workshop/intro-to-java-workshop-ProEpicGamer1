a7c0f1fbb5ce31ce94548d7eed00e4e3f88b0a88
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-26 14:55:16 -0700

    Day2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -6,10 +6,21 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
-		
+	
 		// START HERE
 	
+		System.out.println("Hello World!");
 		
+		Robot sphero = new Robot();
+		sphero.moveTo(200, 500);
+		sphero.setPenColor(0, 0, 250);
+		sphero.setSpeed(999);
+		sphero.setPenWidth(400);
+		sphero.penDown();
 		
+		for (int i = 0; i < 4; i++) {
+			sphero.move(300);
+		sphero.turn(90);
+		}
 	}
 }
