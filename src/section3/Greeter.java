8a08f7f7f314533b0007f2e40a2fa6d9723c6e12
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-27 14:51:59 -0700

    Day3

---
+++ src/section3/Greeter.java
@@ -0,0 +1,13 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	String name =  JOptionPane.showInputDialog("What is your name?");
+System.out.println(name);
+JOptionPane.showMessageDialog(null, "Good Afternoon, " + name);
+JOptionPane.showMessageDialog(null, name + ", you look good today");
+
+}
+}
